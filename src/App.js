a40ad02046117f7ef5d187d3b881ed4b912edf4f
Ali Sariyer <alisariyer@gmail.com>
2022-03-09 21:52:48 +0000

    Add bootstrap npm package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function App() {
-    return (
-        <h1>Hello World</h1>
-    )
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
