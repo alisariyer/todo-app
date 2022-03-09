cf0ec0c2d04d936f925dfca28631555f31d1099f
Ali Sariyer <alisariyer@gmail.com>
2022-03-09 23:12:35 +0000

    Add ListItem component and initial styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="d-flex justify-content-between align-items-start">
+    <header className="d-flex justify-content-between align-items-start mb-5">
       <h1 className="bold text-white m-0">TODO</h1>
       <img className="theme-btn pt-1" src="./images/icon-sun.svg" alt="light theme icon" />
     </header>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,9 @@
 import React from "react";
+import ListItem from "./ListItem";
 
 export default function Input() {
+    const items = [3, 5, 6];
     return (
-        <h2>Hello</h2>
+        <ListItem items={items}/>
     )
 }
